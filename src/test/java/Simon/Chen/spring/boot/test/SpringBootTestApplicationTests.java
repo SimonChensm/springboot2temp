351f130f639856d2624429fa7f4de0c9da8e4d1f
LAPTOP-HLIR3835\Simon.chen <yjes.chen@gmail.com>
2020-04-30 22:45:57 +0800

    initial version

---
+++ src/test/java/Simon/Chen/spring/boot/test/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Simon.Chen.spring.boot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
