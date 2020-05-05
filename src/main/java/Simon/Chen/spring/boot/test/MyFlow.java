8795ac54d1b67808a44dac64c69a90fa7558f8aa
LAPTOP-HLIR3835\Simon.chen <yjes.chen@gmail.com>
2020-05-06 00:14:49 +0800

    MyFlow class adding getter&setter

--- src/main/java/Simon/Chen/spring/boot/test/MyFlow.java
+++ src/main/java/Simon/Chen/spring/boot/test/MyFlow.java
@@ -3,6 +3,7 @@ package Simon.Chen.spring.boot.test;
 public abstract class MyFlow {
 
 	private int b;
+	private float c;
 
 	public int getB() {
 		return b;
@@ -10,6 +11,14 @@ public abstract class MyFlow {
 
 	public void setB(int b) {
 		this.b = b;
+	}
+
+	public float getC() {
+		return c;
+	}
+
+	public void setC(float c) {
+		this.c = c;
 	}
 	
 }
