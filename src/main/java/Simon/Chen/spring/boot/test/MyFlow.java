7d37e2208fec46f15c98ffafd3f3350694a745c8
LAPTOP-HLIR3835\Simon.chen <yjes.chen@gmail.com>
2020-05-05 23:29:53 +0800

    first commit

---
+++ src/main/java/Simon/Chen/spring/boot/test/MyFlow.java
@@ -0,0 +1,15 @@
+package Simon.Chen.spring.boot.test;
+
+public abstract class MyFlow {
+
+	private int b;
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+}
--- src/main/java/Simon/Chen/spring/boot/test/MyObserver.java
+++ src/main/java/Simon/Chen/spring/boot/test/MyObserver.java
@@ -2,4 +2,14 @@ package Simon.Chen.spring.boot.test;
 
 public abstract class MyObserver {
 
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+	
 }
