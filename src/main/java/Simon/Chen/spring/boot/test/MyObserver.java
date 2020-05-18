f49f103f8aeeddc05f4a460a2a0d8aa5169ded85
LAPTOP-HLIR3835\Simon.chen <yjes.chen@gmail.com>
2020-05-18 23:58:02 +0800

    add setter/getter to observer

--- src/main/java/Simon/Chen/spring/boot/test/MyObserver.java
+++ src/main/java/Simon/Chen/spring/boot/test/MyObserver.java
@@ -3,6 +3,7 @@ package Simon.Chen.spring.boot.test;
 public abstract class MyObserver {
 
 	private int a;
+	private String name;
 
 	public int getA() {
 		return a;
@@ -10,6 +11,14 @@ public abstract class MyObserver {
 
 	public void setA(int a) {
 		this.a = a;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
