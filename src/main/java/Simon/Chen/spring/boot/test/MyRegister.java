e74d1e3247b0ef71dfb004c23d1cb182b03b361c
LAPTOP-HLIR3835\Simon.chen <yjes.chen@gmail.com>
2020-05-24 16:25:46 +0800

    Delete s from MyRegister

--- src/main/java/Simon/Chen/spring/boot/test/MyRegister.java
+++ src/main/java/Simon/Chen/spring/boot/test/MyRegister.java
@@ -2,16 +2,7 @@ package Simon.Chen.spring.boot.test;
 
 public abstract class MyRegister {
 
-	private int sn;
 	private String name;
-
-	public int getSn() {
-		return sn;
-	}
-
-	public void setSn(int sn) {
-		this.sn = sn;
-	}
 
 	public String getName() {
 		return name;
